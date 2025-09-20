7469a3801ab95debad0adf23da3071a43d67d4d0
erentsenovsan2008-hue <erentsenovsan2008@gmail.com>
2025-09-20 10:10:24 +0300

    задание 1 с числами задание 2 с строками

---
+++ Number 3.py
@@ -0,0 +1,22 @@
+numbers = [7, 2, 5]
+numbers.append(4)
+numbers.insert(2, 10)
+numbers.extend([1, 1, 1])
+numbers.remove(7)
+pop = numbers[-1]
+numbers.pop(-1)
+numbers.sort()
+numbers.reverse()
+a = numbers.count(2)
+numbers.index(1)
+numbers1 = numbers.copy()
+numbers2 = numbers.copy()
+numbers.clear()
+print(numbers1)
+print(numbers2)
+print(numbers)
+
+
+
+
+
