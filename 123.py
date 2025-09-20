1a7087d3fd16776c658aca0271a4827fa40b64b3
erentsenovsan2008-hue <erentsenovsan2008@gmail.com>
2025-09-20 09:32:27 +0300

    задание 1 с числами

---
+++ 123.py
@@ -0,0 +1,9 @@
+from math import *
+a = float(input("Введите число - "))
+b = int(input("Введите целое число - "))
+print(a+b)
+print(a-b)
+print(a/b)
+R= int(input())
+S = R**2 * pi
+print('Площадб круга - ', S)
